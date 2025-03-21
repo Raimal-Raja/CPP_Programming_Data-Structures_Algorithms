135f53522a3afe324f8e7f3ce1744d78049469d2
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-22 01:52:40 +0500

    initial commit

---
+++ DataType & Operators/02_variables.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+int main(){
+
+    // integer takes 4bytes
+    int age = 7;
+    char b = 'R';
+    float PI = 3.14f; //if you scape to mention f then it will take it as double
+    cout<<age <<endl;
+    //check data types memory
+    cout<<sizeof(age);
+    return 0;
+}
