4320fba1c77c6d36db8a8eda423f4478fc43a77e
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-22 01:32:57 +0500

    initial commit

--- DataType & Operators/01_main.cpp
+++ DataType & Operators/01_main.cpp
@@ -3,5 +3,8 @@ using namespace std; // standard namespace
 
 int main(){
     cout<<"Hello Professor";
+    cout<<"hello Raimal" <<endl;
+    cout<<"hello Raja" <<"\n"; //\n is the slitely faster than endl
+    cout<<'Kolhi\n';
     return 0;
 }
