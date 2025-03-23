0fa66146a5a45945dd6a9c1708821e0c669b1a01
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-23 16:08:26 +0500

    initial commit

--- Patterns/squarePatternCharacter.cpp
+++ Patterns/squarePatternCharacter.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 int main(){
     int n = 4;
-    char ch = 'A';
     for(int i = 0; i<=n-1; i++){
+        char ch = 'A';
         for(int j = 0; j<=n-1; j++){
             cout<<ch<<" ";
+            ch= ch+1;
             
         }
         cout<<endl;
-        ch++;
+        
     }
 
     return 0;
