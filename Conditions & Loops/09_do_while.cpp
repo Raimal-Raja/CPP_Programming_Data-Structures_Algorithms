8f80541d431d91470a9def471db1493ed7de0ca6
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-23 13:53:26 +0500

    initial commit

---
+++ Conditions & Loops/09_do_while.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace  std;
+
+int main(){
+
+    int n = 5;
+    int i = 0;
+
+    do{
+        cout<<i;
+        i++;
+
+    }while(i<=5);
+
+    cout<< endl;
+    return 0;
+}
---
+++ Conditions & Loops/Problems/check_primeNumber.cpp
@@ -0,0 +1,29 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+
+    bool isPrime = true;
+    int n;
+    cout<<"Enter value of N: ";
+    cin>>n;
+
+    for(int i=2; i<=n; i++){
+        if(n%i ==0){
+            isPrime = false;
+            break;
+        }
+        else{
+            isPrime = true;
+        }
+    }
+
+    if(!isPrime){
+        cout<<"it is a Prime number";
+    }
+    else{
+        cout<<"it is not a prime number";
+    }
+
+    return 0;
+}
