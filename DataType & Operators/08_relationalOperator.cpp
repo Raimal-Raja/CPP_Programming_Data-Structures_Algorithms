e587f2094ded14ffe9900cbbdf334fe30ae1cffd
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-23 10:46:25 +0500

    updated

---
+++ DataType & Operators/08_relationalOperator.cpp
@@ -0,0 +1,15 @@
+// relational operators only return true of or false,
+
+# include <iostream>
+using namespace std;
+
+int main(){
+    
+    int a = 9;
+    int b = 3;
+
+    a>b;
+    // greater than, less than, equal to
+
+    return 0;
+}
