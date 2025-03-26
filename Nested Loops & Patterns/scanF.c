73af414e764f55276ad8d6253c03651bd1a5ebdf
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-26 23:12:40 +0500

    initial commit

---
+++ Nested Loops & Patterns/scanF.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+
+    // Input loop
+    for (int i = 0; i < 5; i++) {
+        printf("Enter value of array element %d: ", i + 1);
+        scanf("%d", arr + i);
+    }
+
+    // Output loop
+    printf("Array elements: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    printf("\n"); // Newline for better formatting
+    return 0;
+}
