dbecd9bcb6cf2883783f0c44d52736492a580170
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-28 00:40:26 +0500

    initial commit

--- Functions in C++/function_defination.cpp
+++ Functions in C++/function_defination.cpp
@@ -6,9 +6,19 @@ void printMyName(){
     cout<<"Hello Raimal Raja";
 }
 
-int main(){
+int sumTwoValues(int a, int b){
+    int r = a+b;
+    return r;
+}
 
+int main(){
+    int a,b;
     printMyName();
+    cout<<"\nEnter value of a: ";
+    cin>>a;
+    cout<<"Enter value of b: ";
+    cin>>b;
+    cout<<sumTwoValues(a,b);
 
     return 0;
 }
