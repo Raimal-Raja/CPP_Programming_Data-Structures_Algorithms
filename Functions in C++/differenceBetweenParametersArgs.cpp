9fdc9eb5612367eb76a8154cda35e08677274baa
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-28 00:51:36 +0500

    initial commit

--- Functions in C++/differenceBetweenParametersArgs.cpp
+++ Functions in C++/differenceBetweenParametersArgs.cpp
@@ -1,4 +1,4 @@
 // parameters are copy of argument passed to functions
 // arguments are actual values passed to functions
-// sumTwo(22,44) agruments
+// sumTwo(22,44) agruments, technically there are literals
 // int sumTwo(int a, int b) parameters
