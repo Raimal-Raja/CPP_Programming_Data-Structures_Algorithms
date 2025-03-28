e48a269f4c43f8aeb30942a7a816511089256449
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-28 12:44:45 +0500

    initial commit

---
+++ 06 - DataType Modifiers & Bitwise Operators/bitwiseLeftShift.cpp
@@ -0,0 +1,10 @@
+// left shift = n = 100 = 4  (so 4<<1) =  1000 = 8
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a = 4;
+    cout<<(a<<1);
+    // printf("%d",a);
+    return 0;
+}
---
+++ 06 - DataType Modifiers & Bitwise Operators/bitwiseRightShift.cpp
@@ -0,0 +1,11 @@
+// this will elliment the last one digit
+// 1010 >> 1 = 0101 last one zero will be ellimented
+
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a = 4;
+    cout<<(a>>1);
+    return 0;
+}
