c3498916d9f44c27251aa79a8345d7cfbc9489f3
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-30 03:39:41 +0500

    initial commit

---
+++ 06 - DataType Modifiers & Bitwise Operators/Scope/gloable_scope.cpp
@@ -0,0 +1,13 @@
+// gloable variables = these variables are declared outside of an function or block of code.
+// gloable variables are accessible everwhere in a program.
+
+#include <iostream>
+using namespace std;
+
+int x = 10; // gloable variable
+int main(){
+
+    cout<<x<<endl; // gloable variable is accessible here.
+
+    return 0;
+}
