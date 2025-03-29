86826fca5892d167385b5101a1e888f7aeea08e1
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-03-30 03:25:46 +0500

    initial commit

---
+++ 06 - DataType Modifiers & Bitwise Operators/Operator Precendence/precendence.cpp
@@ -0,0 +1,9 @@
+// precendence = priority
+// Higher priority = !,+,- Right to left
+// *,/% Second higher priority = left to right
+// +, - Third higher priority = left to right 
+// <,<=,>=, > fourth higher priority = Left to  right
+// sixth higher priority ==,!=,
+// && sixth higher priority = left to right
+// || seventh higher priority = Left to right
+// = (assignment operator) last right  to left 
