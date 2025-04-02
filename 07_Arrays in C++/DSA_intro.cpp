27f84605900b0776d7ad299add1ca2b4aedbb13e
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-04-03 03:06:15 +0500

    updated

---
+++ 07_Arrays in C++/DSA_intro.cpp
@@ -0,0 +1,7 @@
+// array is the first data structure
+// data structure are structure in the code when we are programming, which are used  store data
+// all things we are learning (programming, dsa) that we will be able to build real life system we need
+
+// Data Structure  > linear, hierarchyl
+
+//  algorithm, algorthm are different methods to perform effcient operation on data
---
+++ 07_Arrays in C++/array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int mark[5] = {1,2,3,44,55};
+    
+    return 0;
+}
