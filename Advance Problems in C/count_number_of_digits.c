8294ac29fa275facaf56c5c56f56310de78304b3
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-04-10 22:44:08 +0500

    updated

---
+++ Advance Problems in C/count_number_of_digits.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int num = 1000;
+    int count = log10(num)+1;
+
+    printf("%d",count);
+    return 0;
+}
---
+++ Most Advance Techniques/count_number_of_digits.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+#include <math.h>
+int main(){
+    int num = 1000;
+    int count = log10(num)+1;
+    cout<<count;
+    return 0;
+}
