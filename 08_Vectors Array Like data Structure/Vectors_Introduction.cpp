52850e39bca2c7e8146af1a7bbf0237ecbb30bcf
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-04-11 03:56:32 +0500

    updated

---
+++ 08_Vectors Array Like data Structure/Vectors_Introduction.cpp
@@ -0,0 +1,5 @@
+// vectors are dynamic integers, becuase it has not a fixed size,
+// STL a library that contain most of data structure implementation code 
+// STL = standard Template Library 
+// data storing containers are called STL (vector, queue, stack, set)
+//  by default vector is 0
