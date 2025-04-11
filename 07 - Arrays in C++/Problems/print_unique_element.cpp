e4534bc8c7d535a215fc9d7ca99f00b98c6ffe99
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-04-11 14:07:09 +0500

    updated

--- 07 - Arrays in C++/Problems/print_unique_element.cpp
+++ 07 - Arrays in C++/Problems/print_unique_element.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
-#include <vector>
 
 int main(){
     int ans = 0;
-    vector<int>vec = {1,2,3,3,1,2,8};
+    int vec[]= {1,2,3,3,1,2,8};
     for(int val:vec){
         ans = ans^val;
     }
