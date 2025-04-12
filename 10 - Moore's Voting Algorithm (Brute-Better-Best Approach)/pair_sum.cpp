5bc753d8290ba8ae9b6d86992d315df77b3f5052
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-04-12 09:18:00 +0500

    updated

--- 10 - Moore's Voting Algorithm (Brute-Better-Best Approach)/pair_sum.cpp
+++ 10 - Moore's Voting Algorithm (Brute-Better-Best Approach)/pair_sum.cpp
@@ -5,16 +5,19 @@ using namespace std;
 
 int main(){
     int target = 9;
-    vector<int>vec_2;
+    vector<int>vec_index;
+    vector<int>vec_values;
     vector<int>vec = {29,43,11,33,7,5,2};
 
     for(int i = 0; i<vec.size(); i++){
         for(int j = 0; j<vec.size();j++){
             if(target == (vec[i] + vec[j])){
-                vec_2.push_back(j);
+                vec_index.push_back(j);
+                vec_values.push_back(vec[j]);
             }
         }
     }
-    cout<<vec_2[1]<<" "<<vec_2[0];
+    cout<<"found values : "<<vec_values[1]<<" "<<vec_values[0]<<endl;
+    cout<<"found at indicies: "<<vec_index[1]<<" "<<vec_index[0];
     return 0;
 }
