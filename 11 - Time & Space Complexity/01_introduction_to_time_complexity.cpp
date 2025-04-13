452670b57f579e8508df1dcf1462df73dd5bac88
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-04-13 19:35:42 +0500

    updated

---
+++ 11 - Time & Space Complexity/01_introduction_to_time_complexity.cpp
@@ -0,0 +1,10 @@
+// time complexity is not the actual time taken but amount of time taken as function of input size(n).
+// number of operation or process occurs
+// number of input increases it will increase number of operations
+//  y = x 
+// f(x) = x => O(n)
+
+// big O notation give us, worst case sinario or worst case complexity, that is also called (upper bound)
+// worst case complexity  = big O (upper bound)
+// average case complexity = thita
+// best case complexity = omega (lower bound)
---
+++ 11 - Time & Space Complexity/02_introduction_to_space_complexity.cpp
@@ -0,0 +1,4 @@
+// Space Complexity => amount of space taken by an algorithm as function of input size(n)
+// there are two types of space in code, input and auxiliary
+// input space is not counted in space complexity
+// suppose you are writing a program to calculate sum of an array space complexity will be Big O(k) => O(1)
