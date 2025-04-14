442f51b3a814b49809bdd21e0d089afd12a7afee
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-04-14 23:22:56 +0500

    updated

---
+++ 07 - Arrays in C++/Problems/min_max_index.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+# include <vector>
+using namespace std;
+
+vector<int>maxSum(vector<int>nums){
+
+    int n  = nums.size();
+    int ans = INT_MIN;
+    for(int i = 0; i<n; i++){
+        for(int j = i; j<n; j++){
+            if(nums[i]+nums[j] ==ans){
+                ans = nums[i]+nums[j];
+                return ans;
+            }
+        }
+    }
+}
