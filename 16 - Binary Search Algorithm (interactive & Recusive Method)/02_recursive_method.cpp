36f44177c34c30a6e302cbe7eb0c6f5fd8b80680
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-04-27 13:51:48 +0500

    updated

---
+++ 16 - Binary Search Algorithm (interactive & Recusive Method)/02_recursive_method.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int>nums,int target, int left, int right){
+
+    int mid = left + (right - left)/2;
+
+    for(int i = 1; i<nums.size(); i++){
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if( target> nums[mid]){
+           return binarySearch(nums, target, mid +1, right);
+        }
+        else if(target<nums[mid]){
+            return binarySearch(nums, target, left,mid-1);
+        }
+    }
+    return 0;
+}
+
+
+int main(){
+    vector<int>nums = {1,2,3,4,5,6,7};
+    int target = 2;
+    int left =  0;
+    int right = nums.size();
+
+    int r = binarySearch(nums, target, left, right);
+    cout<<r;
+    return 0;
+}
