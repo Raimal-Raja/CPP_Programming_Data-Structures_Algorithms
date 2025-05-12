66b0264d39502f33fa013084d91cc3130a7ec8a6
RAIMAL RAJA <raimalrajagoal@gmail.com>
2025-05-12 09:25:07 +0500

    updated

--- 18 - Sorting Algorithms/01 - Bubble  Sort, Selection  Sort, Insertion Sort/01_bubble_sort.cpp
+++ 18 - Sorting Algorithms/01 - Bubble  Sort, Selection  Sort, Insertion Sort/01_bubble_sort.cpp
@@ -5,14 +5,22 @@
 using namespace  std;
 
 void bubbleSort(vector<int>&arr, int n){
+
+    bool isSwap = false;
     for (int i = 0; i < n-1; i++)
     {
         for (int j = 0; j < n-1; j++)
         {
             if(arr[j]>arr[j+1]){
                 swap(arr[j], arr[j+1]);
+                isSwap = true;
             }
         }
+        if (isSwap)
+        {
+            return;
+        }
+        
         
     }
     
